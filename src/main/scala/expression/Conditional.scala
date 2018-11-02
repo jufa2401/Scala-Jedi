685ce767b6cc0657cf43f2bafadf4e7750c4b63f
jufa2401 <jufa2401@gmail.com>
2018-11-02 14:48:19 -0700

    added most functionality for Jedi 1.0, to be tested and formatted.

--- src/main/scala/expression/Conditional.scala
+++ src/main/scala/expression/Conditional.scala
@@ -6,6 +6,7 @@ import value._
 case class Conditional(val cond: Expression, val cons: Expression, val alt: Expression = null) extends SpecialForm {
   def execute(env: Environment): Value = {
     if(cond.execute(env).asInstanceOf[Boole].value) cons.execute(env)
-    else if(alt == null) Notification.UNSPECIFIED else alt.execute(env)
+    else if(alt == null) Notification.UNSPECIFIED
+    else alt.execute(env)
   }
 }
